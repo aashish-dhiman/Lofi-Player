e7b27c56d5826b98dd4e7c50f4f3bced22d14217
Aashish Dhiman <aashishdhiman88@gmail.com>
2022-12-29 19:50:21 +0530

    added progress bar

--- script/app.js
+++ script/app.js
@@ -4,6 +4,7 @@ const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
+const progress = document.getElementById('progress');
 
 
 playBtn.addEventListener('click', () => (music_play ? pause() : play()));
@@ -76,3 +77,72 @@ function nextSong() {
     }
 }
 
+music.addEventListener('timeupdate', () => {
+    if (music_play) {
+
+        //duration update
+        let duration_sec = music.duration;
+        let sec = Math.floor(duration_sec);
+        let min = Math.floor(sec / 60);
+        sec = Math.round(duration_sec % 60);
+        min = min > 10 ? min : '0' + min;
+        sec = sec > 10 ? sec : '0' + sec;
+        // console.log(music.duration);
+        document.getElementById('duration').innerText = (min + ':' + sec);
+
+        //current time update
+        let current_sec = music.currentTime;
+        let c_sec = Math.floor(current_sec);
+        let c_min = Math.floor(current_sec / 60);
+        c_sec = c_sec % 60;
+        c_min = c_min > 10 ? c_min : '0' + c_min;
+        c_sec = c_sec > 10 ? c_sec : '0' + c_sec;
+        document.getElementById('current-time').innerText = (c_min + ':' + c_sec);
+
+        //setting the progress width
+        let total_duration = music.duration;
+        let value = (current_sec / total_duration) * 100;
+        console.log(value);
+        progress.style.width = value + "%";
+    }
+
+});
+
+//aliter for setting up the duration ,current time and width
+
+
+// music.addEventListener('timeupdate', () => {
+//     //duration update
+//     music.onloadedmetadata = () => {
+//         let duration_sec = music.duration;
+//         let sec = Math.floor(duration_sec);
+//         let min = Math.floor(sec / 60);
+//         sec = Math.round(duration_sec % 60);
+//         min = min > 10 ? min : '0' + min;
+//         sec = sec > 10 ? sec : '0' + sec;
+//         // console.log(music.duration);
+//         document.getElementById('duration').innerText = (min + ':' + sec);
+
+//         //setting the progress width
+//     let total_duration = music.currentTime;
+//     let value = (total_duration / duration_sec) * 100;
+//     console.log(value);
+//     progress.style.width = value + "%";
+//     }
+
+
+//     //current time update
+//     let current_sec = music.currentTime;
+//     let c_sec = Math.floor(current_sec);
+//     let c_min = Math.floor(current_sec / 60);
+//     c_sec = c_sec % 60;
+//     c_min = c_min > 10 ? c_min : '0' + c_min;
+//     c_sec = c_sec > 10 ? c_sec : '0' + c_sec;
+//     document.getElementById('current-time').innerText = (c_min + ':' + c_sec);
+
+//      //setting the progress width
+//      let total_duration = music.duration;
+//      let value = (current_sec / total_duration) * 100;
+//      console.log(value);
+//      progress.style.width = value + "%";
+// });
